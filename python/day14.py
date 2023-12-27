0113891bdd5ff2bb0f688c3949f71ca04ee1ccc9
Theo Crandall <theo.crandall@computronix.com>
2023-12-26 17:29:29 -0700

    Day14 Part 2 Solution

--- python/day14.py
+++ python/day14.py
@@ -2,34 +2,81 @@ from solution import Solution
 
 
 class Day14(Solution):
-    @classmethod
-    def _Part1(cls) -> int:
-        grid = list(map(list, zip(*cls.inputLines)))
-        counts = [0 for _ in range(len(grid))]
+    grid = None
 
-        for line in grid:
+    @classmethod
+    def CalcWeightOnNorth(cls) -> int:
+        return sum(
+            line.count("O") * (len(cls.grid) - i) for i, line in enumerate(cls.grid)
+        )
+
+    @classmethod
+    def TransposeGrid(cls) -> None:
+        cls.grid = tuple(map(tuple, zip(*cls.grid)))
+
+    @classmethod
+    def RotateGrid(cls) -> None:
+        cls.grid = tuple(map(tuple, zip(*cls.grid)))[::-1]
+
+    @classmethod
+    def TiltGridWest(cls) -> None:
+        newGrid = []
+        for line in cls.grid:
+            newLine = ""
             empty = rocks = 0
-            for i, c in enumerate(line):
+            for c in line:
                 if c == ".":
                     empty += 1
                 elif c == "O":
                     rocks += 1
                 elif c == "#":
-                    for j in range(i - empty - rocks, i - empty):
-                        counts[j] += 1
-
+                    newLine += "O" * rocks
+                    newLine += "." * empty
+                    newLine += "#"
                     empty = rocks = 0
 
-            for j in range(len(line) - empty - rocks, len(line) - empty):
-                counts[j] += 1
+            if empty or rocks:
+                newLine += "O" * rocks
+                newLine += "." * empty
 
-        return sum((len(line) - i) * c for i, c in enumerate(counts))
+            newGrid.append(newLine)
+
+        cls.grid = tuple(newGrid)
+
+    @classmethod
+    def _Part1(cls) -> int:
+        cls.grid = tuple(cls.inputLines)
+        cls.TransposeGrid()
+        cls.TiltGridWest()
+        cls.TransposeGrid()
+        return cls.CalcWeightOnNorth()
 
     @classmethod
     def _Part2(cls) -> int:
-        total = 0
+        cls.grid = tuple(cls.inputLines)
+        cls.TransposeGrid()
 
-        return total
+        seen = {}
+        numCycles = 1_000_000_000
+        extraCycles = None
+        for i in range(numCycles):
+            for _ in range(4):
+                cls.TiltGridWest()
+                cls.RotateGrid()
+
+            if cls.grid in seen:
+                if extraCycles is None:
+                    cycleSize = i - seen[cls.grid]
+                    extraCycles = (numCycles - i - 1) % cycleSize - 1
+                elif extraCycles == 0:
+                    break
+                else:
+                    extraCycles -= 1
+            else:
+                seen[cls.grid] = i
+
+        cls.TransposeGrid()
+        return cls.CalcWeightOnNorth()
 
 
 Day14.Run("day14.txt")
